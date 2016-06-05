cdf602575c448882385b98fc30aa9532caa955de
Penthious <tleffew1994@gmail.com>
2016-06-05 12:42:00 -0500

    added seeded model factory and testing index

--- resources/views/forums/index.blade.php
+++ resources/views/forums/index.blade.php
@@ -3,7 +3,17 @@
 
 @section('content')
 
-
+@foreach ($categories as $element)
+	<p>{{$element->name}}</p>
+@endforeach
+<br>
+@foreach ($topics as $element)
+	<p>{{$element->name}}</p>
+@endforeach
+<br>
+@foreach ($posts as $element)
+	<p>{{$element->name}}</p>
+@endforeach
 	<div class="col-md-8 col-md-offset-2">
 		Sample forum show
 	</div>
